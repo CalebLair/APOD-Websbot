501a2d0706e93d6e326a80b324e56c926a6f2ca4
CalebLair <calebsl2@illinois.edu>
2021-06-21 17:21:34 -0500

    Intigrated with tkinter and working with test image

--- APODwebsbot.py
+++ APODwebsbot.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 from requests import get
+import requests
 import textwrap
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+# TODO get and display image
+# TODO display text (and image) with tkinter
 
 
 def cat_clean_print(inp):
@@ -10,10 +17,12 @@ def cat_clean_print(inp):
             return_value += string + " "
         # 180 is full length
         to_print = textwrap.wrap(" ".join(return_value.split()), 180)
+        to_return = ""
         for item in to_print:
-            print(item)
+            to_return += item + "\n"
+        return to_return
     else:
-        print(" ".join(inp.split()))
+        return " ".join(inp.split())
 
 
 if __name__ == "__main__":
@@ -34,11 +43,42 @@ if __name__ == "__main__":
             exp = res[exp_start:i]
             short_stop = res[i]
             rest = res[i+1:]
-    (cat_clean_print(begin))
-    (cat_clean_print(begin_2))
-    (cat_clean_print(date))
-    (cat_clean_print(title))
-    (cat_clean_print(img_credit))
-    (cat_clean_print(exp))
-    (cat_clean_print(short_stop))
-    (cat_clean_print(rest))
+
+    img_tags = page.find_all('img')
+    urls = [img['src'] for img in img_tags]
+    for url in urls:
+        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+        if not filename:
+            print("Regex didn't match with the url: {}".format(url))
+            continue
+        with open(filename.group(1), 'wb') as f:
+            if 'http' not in url:
+                # sometimes an image source can be relative
+                # if it is provide the base url which also happens
+                # to be the site variable atm.
+                url = '{}{}'.format(URL, url)
+            response = requests.get(url)
+            f.write(response.content)
+
+    root = Tk()
+    root.title("APOD webbot")
+    root.iconbitmap("APOD.ico")
+
+    def open():
+        global my_img
+        top = Toplevel()
+        my_img = ImageTk.PhotoImage(Image.open("download.jpg"))
+        testLabel = Label(top, image=my_img)
+        testLabel.pack()
+
+    myLabel1 = Label(root, text=cat_clean_print(begin)).pack()
+    myLabel2 = Label(root, text=cat_clean_print(begin_2)).pack()
+    myLabel3 = Label(root, text=cat_clean_print(date)).pack()
+    myLabel4 = Label(root, text=cat_clean_print(title)).pack()
+    myLabel5 = Label(root, text=cat_clean_print(img_credit)).pack()
+    btn = Button(root, text="Click to show image in new window", command=open).pack()
+    myLabel6 = Label(root, text=cat_clean_print(exp)).pack()
+    myLabel7 = Label(root, text=cat_clean_print(short_stop)).pack()
+    myLabel8 = Label(root, text=cat_clean_print(rest)).pack()
+
+    root.mainloop()
