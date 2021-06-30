9e2865efeeb87efa4ebba7ba0cadc47634d3001f
CalebLair <calebsl2@illinois.edu>
2021-06-30 11:44:20 -0500

    image size problem and wrong file problems fixed but better solutions could be used

--- APODwebsbot.py
+++ APODwebsbot.py
@@ -21,6 +21,19 @@ def cat_clean_print(inp):
         return " ".join(inp.split())
 
 
+def img_open():
+    global my_img
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".jpg"):
+            file_to_use = file
+            top = Toplevel()
+            top.iconbitmap("APOD.ico")
+            # image resizing logic goes here?
+            my_img = ImageTk.PhotoImage(Image.open(file_to_use).resize((400, 400)))
+            testLabel = Label(top, image=my_img)
+            testLabel.pack()
+
+
 if __name__ == "__main__":
     URL = "https://apod.nasa.gov/apod/"
     page_html = get(URL).text
@@ -31,17 +44,25 @@ if __name__ == "__main__":
     begin_2 = res[1:4]
     date = res[4]
     title = res[5]
+
+    exp_start = None
     for i in range(len(res)):
         if "Explanation:" in res[i]:
             img_credit = res[6:i]
             exp_start = i
-        if "Tomorrow's picture:" in res[i]:
+        if "Tomorrow's picture:" in res[i] and exp_start:
             exp = res[exp_start:i]
             short_stop = res[i]
             rest = res[i+1:]
 
     a_things = page.find_all("a")
     urls = [img['href'] for img in a_things]
+
+    # works well enough with download after but could be made better by storing images with dates/filenames
+    # and referring to them by dictionary or other data structure
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith('.jpg'):
+            os.remove(filename)
 
     for url in urls:
         filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
@@ -57,23 +78,12 @@ if __name__ == "__main__":
     root.title("APOD webbot")
     root.iconbitmap("APOD.ico")
 
-
-    def open():
-        global my_img
-        for file in os.listdir(os.getcwd()):
-            if file.endswith(".jpg"):
-                file_to_use = file
-        top = Toplevel()
-        my_img = ImageTk.PhotoImage(Image.open(file_to_use))
-        testLabel = Label(top, image=my_img)
-        testLabel.pack()
-
     myLabel1 = Label(root, text=cat_clean_print(begin)).pack()
     myLabel2 = Label(root, text=cat_clean_print(begin_2)).pack()
     myLabel3 = Label(root, text=cat_clean_print(date)).pack()
     myLabel4 = Label(root, text=cat_clean_print(title)).pack()
     myLabel5 = Label(root, text=cat_clean_print(img_credit)).pack()
-    btn = Button(root, text="Click to show image in new window", command=open).pack()
+    btn = Button(root, text="Click to show image in new window", command=img_open).pack()
     myLabel6 = Label(root, text=cat_clean_print(exp)).pack()
     myLabel7 = Label(root, text=cat_clean_print(short_stop)).pack()
     myLabel8 = Label(root, text=cat_clean_print(rest)).pack()
